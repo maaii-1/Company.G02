8494f0c32e2c0526dc1409da1d1908d9ff29ad7c
Mai <Mai@DESKTOP-MILA5UN>
2025-09-24 09:11:54 +0300

    ...

---
+++ Company.G02.PL/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using Company.G02.BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.G02.PL.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeRepository employeeRepository;
+
+        public EmployeeController(IEmployeeRepository _employeeRepository)
+        {
+            employeeRepository = _employeeRepository;
+        }
+        public IActionResult Index()
+        {
+            var employees = employeeRepository.GetAll();
+            return View(employees);
+        }
+    }
+}
---
+++ Company.G02.PL/Dtos/CreateEmployeeDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Company.G02.PL.Dtos
+{
+    public class CreateEmployeeDto
+    {
+        public string Name { get; set; }
+        public int? Age { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public decimal Salary { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime HiringDate { get; set; }
+        public DateTime CreateAt { get; set; }
+    }
+}
