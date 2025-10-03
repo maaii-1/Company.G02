d7054b2557cf20a2fa04d0c48cda74ef292e249a
Mai <Mai@DESKTOP-MILA5UN>
2025-10-03 06:07:36 +0300

    V04

---
+++ Company.G02.PL/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Company.G02.PL.Controllers
+{
+    public class AccountController : Controller
+    {
+        #region SignUp
+
+        [HttpGet]
+
+
+        #endregion
+    }
+}
